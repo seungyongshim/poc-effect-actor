5cf202125c445ccb87396beb141ff314f6c972c3
SeungYongShim <syshim@me.com>
2023-01-23 22:45:20 +0900

    .

--- src/Effect.Flurl.Tests/UnitTest1.cs
+++ src/Effect.Flurl.Tests/UnitTest1.cs
@@ -1,13 +1,19 @@
+using System.Net;
 using Effect.Abstractions;
 using Flurl.Http;
 using RichardSzalay.MockHttp;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+using static Effect.Flurl.IFlurl<Effect.Flurl.Tests.RT>;
+using System.Diagnostics;
 
 namespace Effect.Flurl.Tests;
-
 public record Response
 {
     public required string Name { get; init; }
 };
+
 
 
 public class FlurlSpec
@@ -32,7 +38,7 @@ public class FlurlSpec
 
         using var cts = new CancellationTokenSource();
 
-        var q = IFlurl<RT>.GetAff<Response>(new("api/user/1234"));
+        var q = GetAff<Response>(new("api/user/1234"));
 
         var ret = await q.Run(new(cts, rest));
 
@@ -45,7 +51,34 @@ public class FlurlSpec
     [Fact]
     public async Task Get400()
     {
+        using var mockHttp = new MockHttpMessageHandler();
 
+        mockHttp.When("http://localhost/api/user/*")
+                .Respond(HttpStatusCode.BadRequest, "application/json", """
+                {
+                    "name" : "Test McGee"
+                }
+                """);
+
+        using var http = mockHttp.ToHttpClient();
+        using var rest = new FlurlClient(http)
+        {
+            BaseUrl = "http://localhost"
+        };
+
+        using var cts = new CancellationTokenSource();
+
+        var q = from res in GetAff<Response>(new("api/user/1234"))
+                select res;
+
+        var ret = await q.Run(new(cts, rest));
+
+        ret.IfFail(err => Debug.WriteLine(err));
+
+        Assert.Equal(ret.ThrowIfFail(), new()
+        {
+            Name = "Value",
+        });
     }
 }
 
--- src/Effect.Flurl/Class1.cs
+++ src/Effect.Flurl/Class1.cs
@@ -2,14 +2,31 @@ using Effect.Abstractions;
 using Flurl.Http;
 using static LanguageExt.Prelude;
 using LanguageExt;
+using LanguageExt.Common;
+using LanguageExt.Pipes;
+using static LanguageExt.Pipes.Pipe;
 
 namespace Effect.Flurl;
 
+public record FlurlError(int Code, dynamic FlurlResponse) : Expected(Code.ToString(), Code, None);
+
 public interface IFlurl<RT> : IHas<RT, FlurlClient> where RT : struct, IFlurl<RT>
 {
+    protected static Producer<RT, IFlurlResponse, Unit> Get(string parameter) =>
+        from http in Eff
+        from ret in Aff(() => http.AllowAnyHttpStatus().Request(parameter).GetAsync().ToValue())
+        from __1 in Producer.yield<RT, IFlurlResponse>(ret)
+        select unit;
+
     public static Aff<RT, T> GetAff<T>(string parameter) =>
         from http in Eff
-        from res in Aff(() => http.Request(parameter).GetAsync().ToValue())
-        from ret in Aff(() => res.GetJsonAsync<T>().ToValue())
+        from ret in use(Aff(() => http.AllowAnyHttpStatus().Request(parameter).GetAsync().ToValue()), res =>
+            from __1 in when(res.StatusCode >= 300,
+                from __1 in Aff(() => res.GetJsonAsync().ToValue())
+                from __2 in FailAff<RT, Unit>(new FlurlError(res.StatusCode, __1))
+                select unit)
+            from ret in Aff(() => res.GetJsonAsync<T>().ToValue())
+            select ret)
         select ret;
 }
+
